b4bc392bbd70c9ceb6f572c4a88194452610cc51
f.chagas <f.chagas@itinovacao.org.br>
2023-02-21 11:59:56 -0300

    feat: noteSchema

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,22 +1,14 @@
-//import "dotenv/config";
 import app from "./app";
 import env from "./util/validadeEnv";
 import mongoose from "mongoose";
 
-
-
-// const port = process.env.PORT;
 const port = env.PORT;
 
-
-// mongoose.connect(process.env.MONGO_CONNECTION_STRING!)
 mongoose.connect(env.MONGO_CONNECTION_STRING)
 .then(()=> { 
-    console.log("mongoose conectado e funcionando");
-
+    console.log("Mongoose conectado");
     app.listen(port, () => {
         console.log("Servidor rodando na porta: " + port);
     });
-}).catch(console.error);
-
-
+})
+.catch(console.error);
